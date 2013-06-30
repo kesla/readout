293192b7f6f1d1e209d54f9bf32fab7c612c2c3f
David Björklund <david.bjorklund@gmail.com>
2013-06-30 13:33:58 +0200

    [feature] Take filename as argument for cmd-tool

--- cmd.js
+++ cmd.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 
-var createReadme = require('./readout')
+var fs = require('fs')
+
+  , createReadme = require('./readout')
+
+  , fileName = process.argv[2]
 
 createReadme(process.cwd(), function(err, readme) {
   if (err)
     throw err
   console.log(readme)
+
+  if (fileName) {
+    fs.writeFile(fileName, readme, function(err) {
+      if (err)
+        throw err
+
+      console.log(fileName + ' written')
+    })
+  }
 })
